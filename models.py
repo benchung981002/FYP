f0784e1d19b9fe04db8c32a7cec1f70f3aa7104f
benchung981002 <44958912+benchung981002@users.noreply.github.com>
2018-12-04 14:56:32 +0800

    Update and rename model.py to models.py

--- models.py
+++ models.py
@@ -1,5 +1,11 @@
-class User(Model):
-    __tablename__ = 'user'
+from flask_appbuilder import Model
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, Date, Float, Text, DateTime
+from sqlalchemy.orm import relationship
+import datetime
+
+
+class MyUser(Model):
+    __tablename__ = 'myuser'
     id = Column(Integer, primary_key=True)
     username = Column(String(50), nullable=False, unique=True)
     password = Column(String(50), nullable=False)
@@ -10,7 +16,7 @@ class User(Model):
 
     def __repr__(self):
         return self.username
-      
+
 class Activity(Model):
     __tablename__ = 'activity'
     id = Column(Integer, primary_key=True)
@@ -19,137 +25,149 @@ class Activity(Model):
     price = Column(Float)
     date = Column(Date)
     place_id = Column(Integer, ForeignKey('place.id'))
-    place = relationship('Place')
+    place = relationship("Place")
     type_id = Column(Integer, ForeignKey('type.id'))
-    type = relationship('Type')
-    
+    type = relationship("Type")
+
     def __repr__(self):
         return self.title
-      
+
+
 class Type(Model):
     __tablename__ = 'type'
     id = Column(Integer, primary_key=True)
     category = Column(String(20))
-  
+
     def __repr__(self):
         return self.category
-      
- class Place(Model):
-     __tablename__ = 'place'
-     id = Column(Integer, primary_key=True)
-     name = Column(String(20))
-     
-     def __repr__(self):
+
+
+class Place(Model):
+    __tablename__ = 'place'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+
+    def __repr__(self):
         return self.name
-  
- class Payment(Model):
+
+
+class Payment(Model):
     __tablename__ = 'payment'
-    id = Column(Integer, primary_key=true) 
+    id = Column(Integer, primary_key=True)
     time = Column(Date)
     activity_id = Column(Integer, ForeignKey('activity.id'))
-    activity = relationship('Acivity')
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user= relationship('User')
-    
+    activity = relationship('Activity')
+    user_id = Column(Integer, ForeignKey('myuser.id'))
+    user = relationship("MyUser")
+
     def __repr__(self):
         return self.time
-      
+
+
 class Forum(Model):
     __tablename__ = 'forum'
-    id = Column(Integer, primary_key=true)
+    id = Column(Integer, primary_key=True)
     category = Column(String(20))
-    
-        def __repr__(self):
-            return self.category
-        
+
+    def __repr__(self):
+        return self.category
+
+
 class Post(Model):
     __tablename__ = 'post'
-    id = Column(Integer, primary_key=true)
+    id = Column(Integer, primary_key=True)
     title = Column(String(50))
     date = Column(Date)
     forum_id = Column(Integer, ForeignKey('forum.id'))
-    forum = relationship('Forum')
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user= relationship('User')
-   
+    forum = relationship("Forum")
+    user_id = Column(Integer, ForeignKey('myuser.id'))
+    user = relationship("MyUser")
+
     def __repr__(self):
         return self.title
-      
+
+
 class Comment(Model):
     __tablename__ = 'comment'
-    id = Column(Integer, primary_key=true)
+    id = Column(Integer, primary_key=True)
     title = Column(String(50))
     content = Column(String(200))
     post_id = Column(Integer, ForeignKey('post.id'))
-    post= relationship('Post')
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user= relationship('User')
-    
-        def __repr__(self):
+    post = relationship("Post")
+    user_id = Column(Integer, ForeignKey('myuser.id'))
+    user = relationship("MyUser")
+
+    def __repr__(self):
         return self.title
-      
+
+
 class Booking(Model):
     __tablename__ = 'booking'
-    id = Column(Integer, primary_key=true)
+    id = Column(Integer, primary_key=True)
     time = Column(Date)
     activity_id = Column(Integer, ForeignKey('activity.id'))
-    activity = relationship('Acivity')
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user= relationship('User')
-    
+    activity = relationship("Activity")
+    user_id = Column(Integer, ForeignKey('myuser.id'))
+    user = relationship("MyUser")
+
     def __repr__(self):
         return self.time
 
- class Login(Model):
+
+class Login(Model):
     __tablename__ = 'login'
-    id = Column(Integer, primary_key=true)
+    id = Column(Integer, primary_key=True)
     time = Column(Date)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user= relationship('User')
-    
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user= relationship('User')
-      
+    user_id = Column(Integer, ForeignKey('myuser.id'))
+    user = relationship("MyUser")
+
+
+    def __repr__(self):
+        return self.time
+
 class FAQ(Model):
     __tablename__ = 'faq'
-    id = Column(Integer, primary_key=true)
-    title = Column(Sting(50))
-    comtent = Column(String(200))
-    
-     def __repr__(self):
-        return self.title 
-      
-  class Feedback(Model):
-    __tablename__ = 'feedback'
-    id = Column(Integer, primary_key=true)
+    id = Column(Integer, primary_key=True)
     title = Column(String(50))
     content = Column(String(200))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user= relationship('User')    
-    
-     def __repr__(self):
-        return self.title
-      
-   class Review(Model):
-     __tablename__ = 'review'
-    id = Column(Integer, primary_key=true)
-    title = Column(String(50))   
-    content = Column(String(200))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user= relationship('User')
-    activity_id = Column(Integer, ForeignKey('activity.id'))
-    activity = relationship('Acivity')
-    rating_id = Column(Integer, ForeignKey('rating.id'))
-    rating = relationship('Rating')
-    
+
     def __repr__(self):
         return self.title
 
- class Rating(Model):
-    __tablename__ = 'rating'
-    id = Column(Integer, primary_key=true)
-    grade = Column(String(20))
-    
-     def __repr__(self):
+
+class Feedback(Model):
+    __tablename__ = 'feedback'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50))
+    content = Column(String(200))
+    user_id = Column(Integer, ForeignKey('myuser.id'))
+    user = relationship("MyUser")
+
+    def __repr__(self):
         return self.title
-     
+
+
+class Review(Model):
+    __tablename__ = 'review'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50))
+    content = Column(String(200))
+    user_id = Column(Integer, ForeignKey('myuser.id'))
+    user = relationship("MyUser")
+    activity_id = Column(Integer, ForeignKey('activity.id'))
+    activity = relationship("Activity")
+    rating_id = Column(Integer, ForeignKey('rating.id'))
+    rating = relationship("Rating")
+
+
+    def __repr__(self):
+        return self.title
+
+
+class Rating(Model):
+    __tablename__ = 'rating'
+    id = Column(Integer, primary_key=True)
+    grade = Column(String(20))
+
+    def __repr__(self):
+        return self.title
